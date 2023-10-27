24d46248754762f5581550e87a2f2fade4f9de8f
posholokia <ilya.posholokk@gmail.com>
2023-10-28 01:13:42 +0600

    added: check password reset link

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,9 +1,11 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import get_user_model
 from djoser.views import UserViewSet
+from djoser.serializers import UidAndTokenSerializer
+from drf_spectacular.utils import extend_schema
 
 User = get_user_model()
 
@@ -13,7 +15,12 @@ class CustomUserViewSet(UserViewSet):
     Вьюсет на базе вьюсета библиотеки Djoser.
     Часть методов переопределена под требования проекта.
     """
-    @action(["post"], detail=False)
+    def get_serializer_class(self):
+        if self.action == 'check_link':
+            return UidAndTokenSerializer
+        return super().get_serializer_class()
+
+    @action(['post'], detail=False)
     def activation(self, request, *args, **kwargs):
         """
         Метод активации учетной записи пользователя.
@@ -31,6 +38,17 @@ class CustomUserViewSet(UserViewSet):
             'refresh': str(refresh),
             'access': str(refresh.access_token),
         }, status=status.HTTP_200_OK)
+
+    @extend_schema(description='Проверка действительности ссылки восстановления пароля')
+    @action(methods=['post'], detail=False, permission_classes=[permissions.AllowAny])
+    def check_link(self, request, *args, **kwargs):
+        """
+        Проверка что ссылка /{uid}/{token}/ действительна, чтобы при повторном переходе по использованной ссылке
+        на фронте сразу вывели сообщение об ошибке, а не после отправки пароля.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     def set_username(self, request, *args, **kwargs):
         """
