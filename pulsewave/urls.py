3778d1a007758f15ee71ac66158464665667f0f0
posholokia <ilya.posholokk@gmail.com>
2023-11-05 18:54:17 +0600

    fixed: urls

--- pulsewave/urls.py
+++ pulsewave/urls.py
@@ -44,4 +44,6 @@ urlpatterns = [
 
 ]
 urlpatterns += router.urls
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
