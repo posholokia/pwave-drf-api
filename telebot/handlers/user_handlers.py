cd8ae8c672e2f98b7ceb9680a20a828f425e5032
ÐAlexander Magomedov <sistem4ik@mail.ru>
2023-11-08 12:40:23 +0500

    add command startbot

--- telebot/handlers/user_handlers.py
+++ telebot/handlers/user_handlers.py
@@ -2,15 +2,14 @@ from aiogram import Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.token_blacklist.management.commands import flushexpiredtokens
 
 from telebot.lexicon.lexicon import LEXICON_RU
 
-#from taskmanager.managers import UserManager
 
-User = get_user_model()
 # Инициализируем роутер уровня модуля
 router = Router()
-
+User = get_user_model()
 
 # Этот хэндлер срабатывает на команду /start
 @router.message(CommandStart())
@@ -18,9 +17,11 @@ async def process_start_command(message: Message):
     await message.answer(text=f"Отлично, {message.from_user.first_name}!!!\n{LEXICON_RU['/start']}")
 
 
-# Этот хэндлер срабатывает на команду /mail
-@router.message(Command(commands='mail'))
-async def process_help_command(message: Message):
-#    if message.text.split()[1] ==
-    print(message.from_user.id, message.text.split()[1])
-    await message.answer(text=LEXICON_RU['mail_changed'])
+# # Этот хэндлер срабатывает на команду /mail
+# @router.message(Command(commands='mail'))
+# async def process_help_command(message: Message):
+# #    if message.text.split()[1] ==
+#     print(message.from_user.id, message.text.split()[1])
+#     await message.answer(text=LEXICON_RU['mail_changed'])
+flushexpiredtokens
+
---
+++ telebot/management/commands/startbot.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from telebot.config_data.config import Config, load_config
+from telebot.handlers import other_handlers, user_handlers
+
+
+# Название класса обязательно - "Command"
+class Command(BaseCommand):
+  	# Используется как описание команды обычно
+    help = 'Just a command for launching a Telegram bot.'
+
+    def handle(self, *args, **kwargs):
+        async def main():
+            # Загружаем конфиг в переменную config
+            config: Config = load_config()
+
+            # Инициализируем бот и диспетчер
+            bot = Bot(token=config.tg_bot.token)
+            dp = Dispatcher()
+
+            # Регистриуем роутеры в диспетчере
+            dp.include_router(user_handlers.router)
+            dp.include_router(other_handlers.router)
+
+            # Пропускаем накопившиеся апдейты и запускаем polling
+            await bot.delete_webhook(drop_pending_updates=True)
+            await dp.start_polling(bot)
+
+        asyncio.run(main())
