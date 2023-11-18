30b80ca020a0d248f2e45cf14b8b07af156e7b48
ÐAlexander Magomedov <sistem4ik@mail.ru>
2023-11-18 14:56:05 +0500

    add new format comments

--- telebot/handlers/user_handlers.py
+++ telebot/handlers/user_handlers.py
@@ -14,15 +14,22 @@ User = get_user_model()
 router = Router()
 
 
-# Этот хэндлер срабатывает на команду /start
+
 @router.message(CommandStart())
 async def process_start_command(message: Message):
+    """
+    Этот хэндлер срабатывает на команду /start.
+    """
     await message.answer(text=f"Отлично, {message.from_user.first_name}!!!\n{LEXICON_RU['/start']}")
 
 
-# Этот хэндлер срабатывает на команду /mail
-@router.message(Command(commands='mail'))
+@router.message(Command(commands='email_add'))
 async def process_mail_command(message: Message):
+    """
+    Этот хэндлер срабатывает на команду /email_add
+    проверяет наличие юзера с такой почтой и при наличии
+    добавляет user_id и telegram_id  в табличку TeleBotID.
+    """
     try:
         if await _email_true(message.text.split()[1]):
             await _save_telegram_id(message)
@@ -33,15 +40,38 @@ async def process_mail_command(message: Message):
         await message.answer(text=LEXICON_RU['mail_empty'])
 
 
-# Проверка наличия указанной в сообщении почты среди почт наших пользователей.
+@router.message(Command(commands='email_delete'))
+async def process_mail_command(message: Message):
+    """
+    Этот хэндлер срабатывает на команду /email_delete
+    проверяет наличие юзера с такой почтой и при наличии
+    удаляет user_id и telegram_id  в из таблички TeleBotID.
+    """
+    try:
+        if await _email_true(message.text.split()[1]):
+            await _save_telegram_id(message)
+            await message.answer(text=LEXICON_RU['mail_changed'])
+        else:
+            await message.answer(text=LEXICON_RU['mail_not'])
+    except IndexError:
+        await message.answer(text=LEXICON_RU['mail_empty'])
+
+
+
 @sync_to_async
 def _email_true(email):
+    """
+    Проверка наличия указанной в сообщении почты среди почт наших пользователей.
+    """
     if email in User.objects.all().values_list('email', flat=True):
         return True
 
-# Сохранение
+
 @sync_to_async
 def _save_telegram_id(message):
+    """
+    Сохраненяет user_id и telegram_id  в табличку TeleBotID.
+    """
     telebotuser=TeleBotID(
         user=User.objects.get(pk=User.objects.get(email=message.text.split()[1]).id),
         telegram_id=message.from_user.id
@@ -49,9 +79,13 @@ def _save_telegram_id(message):
     telebotuser.save()
 
 
-# Этот хэндлер для рассылки всем пользователям бота.
+
 @router.message(Command(commands='sendall'))
 async def send_all(message: Message):
+    """
+    Этот хэндлер срабатывает на команду sendall и рассылает сообщение
+    идущее после команды всем пользователям из таблицы TeleBotID.
+    """
     await message.answer('Начало рассылки ...')
     async for telegram_id in await _give_telebot_id_all():
         await message.bot.send_message(telegram_id['telegram_id'], message.text[message.text.find(' '):])
@@ -60,4 +94,7 @@ async def send_all(message: Message):
 
 @sync_to_async
 def _give_telebot_id_all():
+    """ Возвращает список всех telegram_id из таблицы TeleBotID."""
     return TeleBotID.objects.values('telegram_id')
+
+
