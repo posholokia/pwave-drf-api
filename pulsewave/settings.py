738a0449b1e337f439e482a3aa883f40c6f7f6e7
posholokia <ilya.posholokk@gmail.com>
2023-11-19 23:52:26 +0600

    fixed: settings middleware

--- pulsewave/settings.py
+++ pulsewave/settings.py
@@ -65,8 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'pulsewave.middleware.EnsureSlashMiddleware',
-
 ]
 
 ROOT_URLCONF = 'pulsewave.urls'
