e1b0a53ece02d0a14936950560b93af4ed9b2b41
posholokia <ilya.posholokk@gmail.com>
2023-11-24 21:12:59 +0600

    added: migrations

--- workspaces/models.py
+++ workspaces/models.py
@@ -3,22 +3,22 @@ from pulsewave import settings
 
 
 class WorkSpace(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='space_owner', on_delete=models.CASCADE)
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='workspace_users')
-    invited = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='workspace_invited')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='owned_workspaces', on_delete=models.CASCADE)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='joined_workspaces')
+    invited = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='invited_to_workspaces')
     name = models.CharField('Рабочее пространство', max_length=50)
     created_at = models.DateTimeField('Время создания', auto_now_add=True)
 
 
 class Board(models.Model):
-    work_space = models.ForeignKey(WorkSpace, related_name='board', on_delete=models.CASCADE)
+    work_space = models.ForeignKey(WorkSpace, related_name='boards', on_delete=models.CASCADE)
     name = models.CharField('Доска', max_length=50)
-    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='board_members')
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='joined_boards')
 
 
 class InvitedUsers(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='invited', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='invitations', on_delete=models.CASCADE)
     token = models.CharField(max_length=32)
-    workspace = models.ForeignKey(WorkSpace, related_name='invited_users', on_delete=models.CASCADE)
+    workspace = models.ForeignKey(WorkSpace, related_name='workspace_invitations', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
