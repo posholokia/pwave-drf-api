ee89409091e8e808a79dc166065cc178eced19ee
posholokia <ilya.posholokk@gmail.com>
2023-11-27 21:45:51 +0600

    added: workspace and board api tests; code refactoring

--- pulsewave/settings.py
+++ pulsewave/settings.py
@@ -195,7 +195,7 @@ AUTHENTICATION_BACKENDS = (
 
 #JWT tokens
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(weeks=2),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
