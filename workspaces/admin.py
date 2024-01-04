e19895fed83950b11f2b7fdd562a685f66a34d22
posholokia <ilya.posholokk@gmail.com>
2024-01-05 00:15:03 +0600

    fixed: insert task indexing

--- workspaces/admin.py
+++ workspaces/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import *
 
-# Register your models here.
+User = get_user_model()
+
+
+class WorkSpaceViewAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'name', 'users', 'invited')
+    list_filter = ('owner', 'name', 'users', 'invited')
+    search_fields = ('owner', 'name', 'users', 'invited')
+
+
+admin.site.register(WorkSpace, WorkSpaceViewAdmin)
+admin.site.register(Board)
+admin.site.register(Column)
+admin.site.register(InvitedUsers)
+admin.site.register(Task)
+admin.site.register(Sticker)
