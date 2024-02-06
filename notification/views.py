222edc0333458631b656b7df0ba28e83f2b477d3
posholokia <ilya.posholokk@gmail.com>
2024-02-06 19:49:56 +0600

    added: read all notification api

--- notification/views.py
+++ notification/views.py
@@ -1,9 +1,14 @@
-from rest_framework import viewsets, permissions, status, generics
+from rest_framework import viewsets, permissions, generics
+from rest_framework.decorators import action
+from rest_framework.mixins import ListModelMixin
+
 from . import serializers
 from .models import Notification
+from rest_framework.response import Response
 
 
-class NotificationList(generics.ListAPIView):
+class NotificationList(ListModelMixin,
+                       viewsets.GenericViewSet):
     serializer_class = serializers.NotificationListSerializer
     queryset = Notification.objects.all()
     permission_classes = [permissions.IsAuthenticated]
@@ -14,9 +19,18 @@ class NotificationList(generics.ListAPIView):
         queryset = queryset.filter(recipients=user)
         return queryset.order_by('-created_at')[:25]
 
+    @action(methods=['patch'], detail=False, url_name='read_all_notification')
+    def read_all(self, request, *args, **kwargs):
+        queryset = self.queryset.filter(
+            recipients=self.request.user,
+            read=False,
+        )
+        queryset.update(read=True)
+        data = self.serializer_class(self.get_queryset(), many=True).data
+        return Response(data)
+
 
 class NotificationUpdate(generics.UpdateAPIView):
     serializer_class = serializers.NotificationUpdateSerializer
     queryset = Notification.objects.all()
     permission_classes = [permissions.IsAuthenticated]
-
