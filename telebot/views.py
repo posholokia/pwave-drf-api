513d64a50fd28c062d66e90ee65aea92ddc02fd7
posholokia <ilya.posholokk@gmail.com>
2024-02-07 19:13:12 +0600

    fix log view

--- telebot/views.py
+++ telebot/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 
 def view_logs(request):
     # Чтение содержимого файла логов
-    with open('telegram_bot.log', 'r') as log_file:
-        log_lines = log_file.readlines()
-
+    try:
+        with open('telegram_bot.log', 'r') as log_file:
+            log_lines = log_file.readlines()
+    except FileNotFoundError:
+        with open('telegram_bot.log', 'w') as file:
+            file.write('start log')
+        with open('telegram_bot.log', 'r') as log_file:
+            log_lines = log_file.readlines()
     # Передача содержимого файла логов в шаблон
     return render(request, 'logs.html', {'log_lines': log_lines})
