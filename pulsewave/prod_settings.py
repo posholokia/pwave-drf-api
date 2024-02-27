25d4c41b3111d5b1539a3ed6b0761dc6a753ad65
posholokia <ilya.posholokk@gmail.com>
2024-02-27 23:54:25 +0600

    added sentry

--- pulsewave/prod_settings.py
+++ pulsewave/prod_settings.py
@@ -135,16 +135,16 @@ CACHEOPS = {
 START_BOT_LINK = 'https://t.me/PulseWaveBot?start='
 
 # sentry logging
-# SENTRY = os.getenv('SENTRY')
-# PROJECT = os.getenv('PROJECT')
-#
-# sentry_sdk.init(
-#     # dsn=f"https://c2da026a69c4630d470e2a9c4030d907@o4506724974919680.ingest.sentry.io/4506751797231616",
-#     dsn=f"https://{SENTRY}.ingest.sentry.io/{PROJECT}",
-#     integrations=[
-#         LoggingIntegration(
-#             level=logging.ERROR,        # Capture info and above as breadcrumbs
-#             event_level=logging.ERROR   # Send records as events
-#         ),
-#     ],
-# )
+SENTRY = os.getenv('SENTRY')
+PROJECT = os.getenv('PROJECT')
+
+sentry_sdk.init(
+    # dsn=f"https://c2da026a69c4630d470e2a9c4030d907@o4506724974919680.ingest.sentry.io/4506751797231616",
+    dsn=f"https://{SENTRY}.ingest.sentry.io/{PROJECT}",
+    integrations=[
+        LoggingIntegration(
+            level=logging.ERROR,        # Capture info and above as breadcrumbs
+            event_level=logging.ERROR   # Send records as events
+        ),
+    ],
+)
