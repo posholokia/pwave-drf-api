9e4daf126eea52917528538ccb932e369df9084d
posholokia <ilya.posholokk@gmail.com>
2024-03-18 15:54:17 +0600

    fixed: db_dump; added: runner tags

--- ci/db_dump.sh
+++ ci/db_dump.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 DUMPFILE=$PGDATABASE_$(date +%Y-%m-%d_%H:%M:%S).dump;
+echo "--$PGDATABASE--"
 docker exec -it $PGCONTAINER bash -c "pg_dump -U $PGUSER -w -Fc -x $PGDATABASE -f /tmp/dumpfile.dump";
-docker cp $PGCONTAINER:/tmp/dumpfile.dump /tmp/$DUMPFILE
-docker exec -it $PGCONTAINER rm /tmp/$DUMPFILE;
+docker cp $PGCONTAINER:/tmp/dumpfile.dump ~/tmp/$DUMPFILE
+docker exec -it $PGCONTAINER rm /tmp/dumpfile.dump;
 
 printf "%s\n" "$AWSKEY" "$AWSSECRET" "$AWSREGION" "" | aws configure
 aws s3 --endpoint-url=https://storage.yandexcloud.net cp /tmp/$DUMPFILE s3://$AWSBUCKET/postgresql/db_dumps/$DUMPFILE;
@@ -12,4 +13,4 @@ aws s3 ls  --endpoint-url=https://storage.yandexcloud.net s3://$AWSBUCKET/postgr
   tail -n "+$(($BACKUPS_QUANTITY + 1))"|\
   xargs -I FILE aws s3 rm --endpoint-url=https://storage.yandexcloud.net s3://$AWSBUCKET/postgresql/db_dumps/FILE;
 
-rm /tmp/$DUMPFILE;
+rm ~/tmp/$DUMPFILE;
