2f4aef5e22d914b7394856389eb5468d544f8719
posholokia <ilya.posholokk@gmail.com>
2024-03-18 15:31:10 +0600

    fixed: db_dump; added: runner tags

--- ci/db_dump.sh
+++ ci/db_dump.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 DUMPFILE=$PGDATABASE_$(date +%Y-%m-%d_%H:%M:%S).dump;
-docker exec -it $PGCONTAINER bash -c pg_dump -Fc -x $PGDATABASE > /tmp/$DUMPFILE;
+docker exec -it $PGCONTAINER bash -c "pg_dump -U $PGUSER -w -Fc -x $PGDATABASE -f /tmp/dumpfile.dump";
+docker cp $PGCONTAINER:/tmp/dumpfile.dump /tmp/$DUMPFILE
+docker exec -it $PGCONTAINER rm /tmp/$DUMPFILE;
+
 printf "%s\n" "$AWSKEY" "$AWSSECRET" "$AWSREGION" "" | aws configure
 aws s3 --endpoint-url=https://storage.yandexcloud.net cp /tmp/$DUMPFILE s3://$AWSBUCKET/postgresql/db_dumps/$DUMPFILE;
 # Remove old backups
 aws s3 ls  --endpoint-url=https://storage.yandexcloud.net s3://$AWSBUCKET/postgresql/db_dumps --recursive |\
   sort -r|sed -E "s|.*$PGDATABASE/(.*)\$|\1|g;/^\$/d"|\
-  tail -n "+$((1))"|\
+  tail -n "+$(($BACKUPS_QUANTITY + 1))"|\
   xargs -I FILE aws s3 rm --endpoint-url=https://storage.yandexcloud.net s3://$AWSBUCKET/postgresql/db_dumps/FILE;
+
+rm /tmp/$DUMPFILE;
