02c57125db89981f8bb7ea105c3e82d90e1c4690
posholokia <ilya.posholokk@gmail.com>
2024-03-18 17:19:45 +0600

    fixed: remove old damps

--- ci/db_dump.sh
+++ ci/db_dump.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-DUMPFILE=$PGDATABASE_$(date +%Y-%m-%d_%H:%M:%S).dump;
+DUMPFILE=${PGDATABASE}_$(date +%Y-%m-%d_%H:%M:%S).dump;
 echo $PGPASS | docker exec -i $PGCONTAINER bash -c "pg_dump -U $PGUSER -W -Fc -x $PGDATABASE -f /tmp/dumpfile.dump";
 docker cp $PGCONTAINER:/tmp/dumpfile.dump ~/tmp/$DUMPFILE
 docker exec -i $PGCONTAINER rm /tmp/dumpfile.dump;
 
 printf "%s\n" "$AWSKEY" "$AWSSECRET" "$AWSREGION" "" | aws configure
-aws s3 --endpoint-url=https://storage.yandexcloud.net cp ~/tmp/$DUMPFILE s3://$AWSBUCKET/postgresql/db_dumps/$DUMPFILE;
+aws s3 --endpoint-url=https://storage.yandexcloud.net cp ~/tmp/$DUMPFILE s3://$AWSBUCKET/postgresql/test_db_dumps/$DUMPFILE;
+
 # Remove old backups
-aws s3 ls  --endpoint-url=https://storage.yandexcloud.net s3://$AWSBUCKET/postgresql/db_dumps --recursive |\
-  sort -r|sed -E "s|.*$PGDATABASE/(.*)\$|\1|g;/^\$/d"|\
-  tail -n "+$(($BACKUPS_QUANTITY + 1))"|\
-  xargs -I FILE aws s3 rm --endpoint-url=https://storage.yandexcloud.net s3://$AWSBUCKET/postgresql/db_dumps/FILE;
+aws s3 ls --endpoint-url=https://storage.yandexcloud.net s3://$AWSBUCKET/postgresql/db_dumps --recursive |\
+  awk '{print $NF}' | sort -r | sed -E "s|.*$PGDATABASE/(.*)\$|\1|g;/^\$/d" |\
+  tail -n "+$(($BACKUPS_QUANTITY + 1))" |\
+  xargs -I FILE aws s3 rm --endpoint-url=https://storage.yandexcloud.net s3://$AWSBUCKET/FILE;
 
 rm ~/tmp/$DUMPFILE;
