d60397f5e9d58643c62f29b28c8e87bec0c95174
posholokia <ilya.posholokk@gmail.com>
2024-03-29 22:19:31 +0600

    fixed: django docker up

--- pulsewave/settings.py
+++ pulsewave/settings.py
@@ -60,7 +60,6 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
-    'django_grip.GripMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
